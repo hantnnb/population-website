39ec2c56a2ac04740dce4e0b4f03034cf6e194e6
hantnnb <han.tnnb@gmail.com>
2025-09-11 11:23:07 +0700

    adding @ before email

--- terraform/envs/dev/monitors-dev/main.tf
+++ terraform/envs/dev/monitors-dev/main.tf
@@ -2,7 +2,7 @@ module "gce_cpu_high" {
   source = "../../../modules/datadog-monitors/gce_cpu_high"
   # env    = "dev"
   tag_filter         = "host:pplt-dev-vm*"
-  notify             = "${var.notify} @webhook.${datadog_webhook.discord.name}"
+  notify             = "@${var.notify} @webhook.${datadog_webhook.discord.name}"
   warning_threshold  = 80
   critical_threshold = 95
 }
