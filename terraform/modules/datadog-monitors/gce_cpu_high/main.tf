de444902625f4889665bff6a2897148e13a4a691
hantnnb <han.tnnb@gmail.com>
2025-09-11 10:45:37 +0700

    removing tag env

--- terraform/modules/datadog-monitors/gce_cpu_high/main.tf
+++ terraform/modules/datadog-monitors/gce_cpu_high/main.tf
@@ -1,6 +1,6 @@
 # CPU used % = 100 - cpu.idle
 resource "datadog_monitor" "gce_cpu_high" {
-  name = "[${var.env}] GCE CPU usage high"
+  name = "GCE CPU usage high"
   type = "metric alert"
 
   # Example query:
