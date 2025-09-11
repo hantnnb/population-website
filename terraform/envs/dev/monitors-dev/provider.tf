317d700d5959317f3d7c6e1cd571219da3e03000
hantnnb <han.tnnb@gmail.com>
2025-09-11 10:13:42 +0700

    adding github secret for discord webhook url

--- terraform/envs/dev/monitors-dev/provider.tf
+++ terraform/envs/dev/monitors-dev/provider.tf
@@ -8,36 +8,6 @@ terraform {
   }
 }
 
-
-variable "datadog_api_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "datadog_app_key" {
-  type      = string
-  sensitive = true
-}
-
-variable "datadog_api_url" {
-  type    = string
-  default = "ap1.datadoghq.com"
-
-}
-
-variable "notify" {
-  type    = string
-  default = "han.tnnb@gmail.com"
-}
-
-variable "name_prefix" {
-  type = string
-}
-
-variable "discord_webhook_url" {
-  type = string
-}
-
 provider "datadog" {
   api_key = var.datadog_api_key
   app_key = var.datadog_app_key
---
+++ terraform/envs/dev/monitors-dev/variables.tf
@@ -0,0 +1,28 @@
+variable "datadog_api_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_app_key" {
+  type      = string
+  sensitive = true
+}
+
+variable "datadog_api_url" {
+  type    = string
+  default = "ap1.datadoghq.com"
+
+}
+
+variable "notify" {
+  type    = string
+  default = "han.tnnb@gmail.com"
+}
+
+variable "name_prefix" {
+  type = string
+}
+
+variable "discord_webhook_url" {
+  type = string
+}
